d3ccf2e966ea9ab896ab94f641093cf82700b719
kenalOrtega <kenalortega@gmail.com>
2020-08-05 17:12:16 -0400

    updated table

--- app.js
+++ app.js
@@ -10,17 +10,65 @@ const $createpositionInput = $("#createposition");
 const $createdateInput = $("#createdate");
 const $createlogInput = $("#createlog")
 const $button = $("#createUserBtn");
+const $button2 = $("#createUserBtn2");
 
+const $editbutton = $("#editbtn");
+const $deletebutton = $("deletebtn");
+
+//const $logSelect = $("#createselect");
+//const $logEditInput = $("#editinput")
+
+const $userSelect = $("#chooseUser1")
+
+
+const $tbody = $("#tbody")
+
+
+
+
+
+const tableFiller = () => {
+  const $tr = $("<tr>")
+const $th = $("<th>")
+$tr.append($tr)
+const $td = $("<td>")
+const $tdb = $("<td>")
+
+$tr.append($td)
+  $tbody.append($tr)
+$tr.append($th)
+$tr.append($td)
+$tr.append($("<td>"))
+$tr.append($("<td>"))
+$tr.append($("<td>"))
+$tr.append($tdb.append($("<button class='btn btn-primary'>").text("Edit")))
+$tr.append($tdb.append($("<button class='btn btn-danger'>").text("Delete")))
+}
+tableFiller()
+
+
+
+
+const userChoice = async () => {
+  const response = await fetch(`${URL}findusers`)
+  const data = await response.json();
+  data.forEach((user) => {
+    const $option = $('<option>').attr("value", user._id).text(`${user.Name}, ${user.Position}`);
+     $userSelect.append($option); 
+  })
+};
+userChoice()
 
 
 //create user
-const createUser = async () => {
+const createUser = async (event) => {
+  event.preventDefault()
     
     const newUser = {
       Name: $createuserInput.val(),
       Position: $createpositionInput.val()
     }
-    //console.log(newUser)
+    console.log(newUser)
     //send request to api to create 
     const response = await fetch(`${URL}user`, {
       method: "post",
@@ -28,33 +76,36 @@ const createUser = async () => {
         "Content-Type":"application/json"
       },
       body: JSON.stringify(newUser)
+      
     })
     const data = await response.json();
     console.log(data)
-
+    $userSelect.empty()
+    userChoice()
+   
   };
   $button.on("click", createUser)
+  
 
-
-  //create log
-const createLog = async () => {
+   //create log
+const createLog = async (event) => {
+  event.preventDefault()
     
-    const newLog = {
-      Date: $createdateInput.val(),
-      Log: $createlogInput.val()
-    }
-    //console.log(newUser)
-    //send request to api to create 
-    const response = await fetch(`${URL}log`, {
-      method: "post",
-      headers: {
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify(newLog)
-    })
-    const data = await response.json();
-    console.log(data)
+  const newLog = {
+    Date: $createdateInput.val(),
+    Log: $createlogInput.val()
+  }
+  //console.log(newUser)
+  //send request to api to create 
+  const response = await fetch(`${URL}log`, {
+    method: "post",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(newLog)
+  })
+  const data = await response.json();
+  console.log(data)
 
-  };
-  $button.on("click", createLog)
-
+};
+$button2.on("click", createLog)
